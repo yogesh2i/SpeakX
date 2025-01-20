1cd2271d061450d07996682f78f9f9ed815b22f4
yogesh <yogirao8440@gmail.com>
2025-01-20 20:10:07 +0530

    fix: api delay reduced

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -1,19 +1,17 @@
-import React, { useContext} from 'react'
+import React, { useContext } from 'react'
 import Style from './Search.module.scss';
 import { SearchContext } from '../../Context/QuestionContext';
+
 export default function Search() {
-    const {query,setQuery,setPage} = useContext(SearchContext);
-     
+  const { query, setQuery, setPage } = useContext(SearchContext);
 
-
-  function handleQuery(e){
+  const handleQuery =(e)=> {
     setPage(1);
-     setQuery(e.target.value);
+    setQuery(e.target.value);
   }
   return (
     <>
-       <input type="text" name="query" id="query"  placeholder='Enter Title Here' className={Style.query} value={query} onChange={(e)=>handleQuery(e)}/>
-
+      <input type="text" name="query" id="query" placeholder='Enter Title Here' className={Style.query} value={query} onChange={(e) => handleQuery(e)} />
     </>
   )
 }
