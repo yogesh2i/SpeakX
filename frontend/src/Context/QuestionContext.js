8faef9e942c9dd0224c100628eed1c69fa6dda3f
yogesh <yogirao8440@gmail.com>
2025-01-20 20:12:03 +0530

    update: api url updated

--- frontend/src/Context/QuestionContext.js
+++ frontend/src/Context/QuestionContext.js
@@ -7,21 +7,21 @@ export const SearchProvider = ({ children }) => {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
   const [data, setData] = useState([]);
-  const [filter,setFilter] = useState("All");
- 
-  useEffect(()=>{
-    const searchData = setTimeout(()=>{
-        fetch(`${process.env.REACT_APP_FETCH_URL}/questions?limit=${limit}&page=${page}&query=${query}&filter=${filter}`,{
-        }).then((res)=>{
+  const [filter, setFilter] = useState("All");
+
+  useEffect(() => {
+    const searchData = setTimeout(() => {
+      fetch(`${process.env.REACT_APP_FETCH_URL}/questions?limit=${limit}&page=${page}&query=${query}&filter=${filter}`)
+        .then((res) => {
           return res.json();
-        }).then((result)=>{
+        }).then((result) => {
           setData(result);
-        }).catch((err)=>{
-            console.log(err)
+        }).catch((err) => {
+          console.log(err)
         })
-    },1000)
-     return (()=>clearTimeout(searchData));
-  },[query,filter,page,limit])
+    }, 700)
+    return (() => clearTimeout(searchData));
+  }, [query, filter, page, limit])
 
   return (
     <SearchContext.Provider
