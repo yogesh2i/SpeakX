c371931ea33a0c4ae64691c3af1d3425dddba479
yogesh <yogirao8440@gmail.com>
2025-01-20 20:11:25 +0530

    feat: filters color added

--- frontend/src/Components/Filters/Filters.jsx
+++ frontend/src/Components/Filters/Filters.jsx
@@ -2,42 +2,48 @@ import React, { useContext, useState } from 'react';
 import Style from './Filters.module.scss';
 import { SearchContext } from '../../Context/QuestionContext';
 
-export default function Filters() {
-    const {setFilter,setPage} = useContext(SearchContext);
-    const filters = [
-        {"text": "All",
-         "value": "All"
-        },
-        {"text": "mcq",
-         "value": "MCQ"
-        },
-        {"text": "Read along",
-         "value": "READ_ALONG"
-        },
-        {"text": "Content only",
-         "value": "CONTENT_ONLY"
-        },
-        {"text": "Anagram",
-         "value": "ANAGRAM"
-        }
-    ]
-    const [crntBtn,setCrntBtn] = useState(0);
+const filters = [
+  {
+    "text": "All",
+    "value": "All"
+  },
+  {
+    "text": "mcq",
+    "value": "MCQ"
+  },
+  {
+    "text": "Read along",
+    "value": "READ_ALONG"
+  },
+  {
+    "text": "Content only",
+    "value": "CONTENT_ONLY"
+  },
+  {
+    "text": "Anagram",
+    "value": "ANAGRAM"
+  }
+];
 
- function handleClick(index){
-    setCrntBtn(index);
+export default function Filters() {
+  const { setFilter, setPage } = useContext(SearchContext);
+  const [currentBtn, setCurrentBtn] = useState(0);
+
+  function handleClick(index) {
+    setCurrentBtn(index);
     setPage(1);
     setFilter(filters[index].value);
- }
+  }
 
   return (
     <div className={Style.filters}>
-       <div>
-        {filters.map((i,index)=>{
+      <div>
+        {filters.map((i, index) => {
           return (
-          <button onClick={()=>handleClick(index)} className={crntBtn===index?Style.current:''} key={i.value}>{i.text}</button>
-        )
-        })}        
-       </div>
+            <button onClick={() => handleClick(index)} className={currentBtn === index ? Style.current : ''} key={i.value}>{i.text}</button>
+          )
+        })}
+      </div>
     </div>
   )
 }
